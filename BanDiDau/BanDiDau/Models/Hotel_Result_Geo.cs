3309cb814b8e20048d579f1ff436ed87e048a15c
Pham Trung KIen <kienpt2015@outlook.com>
2016-01-07 20:50:01 +0700

    Hotel-list-room

--- BanDiDau/BanDiDau/Models/Hotel_Result_Geo.cs
+++ BanDiDau/BanDiDau/Models/Hotel_Result_Geo.cs
@@ -8,7 +8,7 @@ namespace BanDiDau.Models
 {
     public class Hotel_Result_Geo
     {
-        public string name{get; set;}
+        public string name { get; set; }
         public float price { get; set; }
         public float rate { get; set; }
         public string location { get; set; }
@@ -17,10 +17,12 @@ namespace BanDiDau.Models
         public string category { get; set; }
         public string cxDeadline { get; set; }
         public string urlImageRate { get; set; }
-        public string hotelSearchCode{get; set;}
+        public string hotelSearchCode { get; set; }
+        public string description { get; set; }
         public int hotelCode { get; set; }
         public int cityCode { get; set; }
         public string thumbnail { get; set; }
-        
+        public string roomType { get; set; }
+
     }
 }
